0116f27b6ab2b2cbfd38cf369bf1251591704afc
Michael Stucki <michael.stucki@typo3.org>
2007-10-17 09:05:06 +0000

    Takeover by the DAM development team
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_ttcontent/branches/dam_ttcontent_1-0@7005 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Modify the content types "Image" and "Text/Image" for usage of the DAM. This depends on TYPO3 4.1 or the extension \'mmforeign\'.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.2',
+	'version' => '1.0.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -27,24 +27,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => 'tt_content',
 	'clearcacheonload' => 0,
 	'lockType' => '',
-	'author' => 'Rene Fritz',
-	'author_email' => 'r.fritz@colorcube.de',
-	'author_company' => 'Colorcube - digital media lab, www.colorcube.de',
+	'author' => 'The DAM development team',
+	'author_email' => 'typo3-project-dam@lists.netfielders.de',
+	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
 			'dam' => '',
-			'php' => '4.0.0-0.0.0',
-			'typo3' => '3.8.0-0.0.0',
+			'php' => '4.0.0-',
+			'typo3' => '3.8.0-',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"d1ab";s:20:"class.ext_update.php";s:4:"ea51";s:21:"ext_conf_template.txt";s:4:"f401";s:12:"ext_icon.gif";s:4:"999b";s:17:"ext_localconf.php";s:4:"ce88";s:14:"ext_tables.php";s:4:"f3e4";s:14:"ext_tables.sql";s:4:"aa4c";s:14:"doc/manual.sxw";s:4:"6056";}',
+	'_md5_values_when_last_written' => '',
 );
 
 ?>
